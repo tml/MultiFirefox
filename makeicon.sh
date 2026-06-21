c71eda496bd9724f6cc50868d21df998ed4d5bf4
Joey Smith <joeysmith@gmail.com>
2026-06-21 17:24:14 -0600

    makeicon.sh: fix to use modern inkscape CLI flags

--- makeicon.sh
+++ makeicon.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 
 # Make .icns file from mozilla's firefox svg icon asset (CC-BY-SA 3.0)
-# Prereqs: curl, inkscape and pngcrush (brew cask install inkscape && brew install pngcrush) 
+# Prereqs: curl, inkscape and pngcrush (brew install inkscape pngcrush)
 if [ ! -f firefox-logo.svg ]; then
-  curl -L -O http://design.firefox.com/product-identity/firefox/firefox/firefox-logo.svg
+  curl -L -o firefox-logo.svg https://www.mozilla.org/media/protocol/img/logos/firefox/browser/logo.svg
 fi
 mkdir firefox.iconset
-inkscape -z -e $(pwd)/firefox.iconset/icon_32x32.png -w 32 -h 32 $(pwd)/firefox-logo.svg
+inkscape --export-filename="$(pwd)/firefox.iconset/icon_32x32.png" --export-width=32 --export-height=32 "$(pwd)/firefox-logo.svg"
 pngcrush -ow firefox.iconset/icon_32x32.png
-inkscape -z -e $(pwd)/firefox.iconset/icon_32x32@2x.png -w 64 -h 64 $(pwd)/firefox-logo.svg
+inkscape --export-filename="$(pwd)/firefox.iconset/icon_32x32@2x.png" --export-width=64 --export-height=64 "$(pwd)/firefox-logo.svg"
 pngcrush -ow firefox.iconset/icon_32x32@2x.png
-inkscape -z -e $(pwd)/firefox.iconset/icon_128x128.png -w 128 -h 128 $(pwd)/firefox-logo.svg
+inkscape --export-filename="$(pwd)/firefox.iconset/icon_128x128.png" --export-width=128 --export-height=128 "$(pwd)/firefox-logo.svg"
 pngcrush -ow firefox.iconset/icon_128x128.png
-inkscape -z -e $(pwd)/firefox.iconset/icon_128x128@2x.png -w 256 -h 256 $(pwd)/firefox-logo.svg
+inkscape --export-filename="$(pwd)/firefox.iconset/icon_128x128@2x.png" --export-width=256 --export-height=256 "$(pwd)/firefox-logo.svg"
 pngcrush -ow firefox.iconset/icon_128x128@2x.png
-inkscape -z -e $(pwd)/firefox.iconset/icon_512x512.png -w 512 -h 512 $(pwd)/firefox-logo.svg
+inkscape --export-filename="$(pwd)/firefox.iconset/icon_512x512.png" --export-width=512 --export-height=512 "$(pwd)/firefox-logo.svg"
 pngcrush -ow firefox.iconset/icon_512x512.png
-inkscape -z -e $(pwd)/firefox.iconset/icon_512x512@2x.png -w 1024 -h 1024 $(pwd)/firefox-logo.svg
+inkscape --export-filename="$(pwd)/firefox.iconset/icon_512x512@2x.png" --export-width=1024 --export-height=1024 "$(pwd)/firefox-logo.svg"
 pngcrush -ow firefox.iconset/icon_512x512@2x.png
 iconutil --convert icns firefox.iconset
 rm -rf firefox.iconset
